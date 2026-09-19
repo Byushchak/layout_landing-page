4721902db0112f0315b3a29328f21f708fed778b
Bohdan Yushchak <bogdan10022004@gmail.com>
2026-09-19 16:32:50 +0200

    My landing page

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,40 @@
 'use strict';
+
+document.addEventListener('click', documentActions);
+
+function documentActions(e) {
+  const targetElement = e.target;
+
+  if (targetElement.closest('.menu__icon')) {
+    document.documentElement.classList.toggle('menu-open');
+  }
+
+  if (targetElement.closest('.menu__link')) {
+    document.documentElement.classList.remove('menu-open');
+  }
+}
+
+/* global IntersectionObserver */
+
+const options = {
+  root: null,
+  rootMargin: '0px',
+  threshold: 0.2,
+};
+
+const callback = (entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('--animate');
+      observer.unobserve(entry.target);
+    }
+  });
+};
+
+const observer = new IntersectionObserver(callback, options);
+
+const animElements = document.querySelectorAll('[class*="--anim"]');
+
+animElements.forEach((animElement) => {
+  observer.observe(animElement);
+});
